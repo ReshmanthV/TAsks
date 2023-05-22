fdf8de892f578429a69ac4fa2f5cabbdac6bf0f7
ReshmanthV <132649903+ReshmanthV@users.noreply.github.com>
2023-05-22 18:14:34 +0530

    Delete grocery_list1 directory

--- grocery_list1/main.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-
-from scripts.services.gt_services import gt_router
-
-app = FastAPI()
-
-app.include_router(gt_router)
--- grocery_list1/scripts/constants/__init__.py
+++
@@ -1,6 +0,0 @@
-from scripts.config import Service
-
-
-class DatabaseConstants:
-    database_name = Service.database_name
-    collection_name = Service.collection_name
